37f3e33119569f389ae93919a81a00a367c81628
Charles Washington <charles_was_7@hotmail.com>
2019-01-31 11:19:44 -0300

    Add basic models to the project.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Creating Statuses'
+
+Status.find_or_create_by!(description: 'To Do')
+Status.find_or_create_by!(description: 'Doing')
+Status.find_or_create_by!(description: 'Done')
+
+puts 'Creating a Few Tasks'
+
+Task.find_or_create_by!(title: 'Study Neural Networks',
+                        body: 'AI is cool. Learn more about it.',
+                        status: Status.first)
+
+Task.find_or_create_by!(title: 'Develop RoR sample application',
+                        body: 'DO IT.',
+                        status: Status.first)
+
+Task.find_or_create_by!(title: 'Sleep',
+                        body: 'Shouldn\'t be too hard',
+                        status: Status.second)
+
+Task.find_or_create_by!(title: 'Brush your teeth every day',
+                        body: 'Yeah.',
+                        status: Status.third)
