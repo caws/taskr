b80d50158fa2146a5571fab1e992c779323c117f
Charles Washington <charles_was_7@hotmail.com>
2019-02-01 00:25:20 -0300

    Add new, create and destroy actions when dealing with a Task. Add bootstrap modal. Organize the code a bit.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
   def index
+    @statuses = Status.all
   end
 end
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -28,10 +28,10 @@ class TasksController < ApplicationController
 
     respond_to do |format|
       if @task.save
-        format.html { redirect_to @task, notice: 'Task was successfully created.' }
+        format.js
         format.json { render :show, status: :created, location: @task }
       else
-        format.html { render :new }
+        format.js { render :new }
         format.json { render json: @task.errors, status: :unprocessable_entity }
       end
     end
@@ -56,7 +56,8 @@ class TasksController < ApplicationController
   def destroy
     @task.destroy
     respond_to do |format|
-      format.html { redirect_to tasks_url, notice: 'Task was successfully destroyed.' }
+      format.js
+      # format.html { redirect_to tasks_url, notice: 'Task was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
