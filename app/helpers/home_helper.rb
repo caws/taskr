4eba46684f96167fa5140675767f825ae866d31f
Charles Washington <charles_was_7@hotmail.com>
2019-02-01 11:55:47 -0300

    Update task status with the touch of a button.

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,9 @@
 module HomeHelper
+  def show_anchors task
+    todo = task.status == Status.first
+    doing = task.status == Status.second
+    done = task.status == Status.third
+
+    return todo, doing, done
+  end
 end
