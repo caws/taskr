fa6722c6c8d93a71b7b7e11f95af2e98cc5b58cb
Charles Washington <charles_was_7@hotmail.com>
2019-02-01 23:56:29 -0300

    Bring back lines that shouldn't have been deleted.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -29,6 +29,7 @@ class TasksController < ApplicationController
     respond_to do |format|
       if @task.save
         format.js
+        format.html { redirect_to @task, notice: 'Task was successfully created.' }
         format.json { render :show, status: :created, location: @task }
       else
         format.js { render :new }
@@ -43,11 +44,11 @@ class TasksController < ApplicationController
     respond_to do |format|
       if @task.update(task_params)
         format.js
-        # format.html { redirect_to @task, notice: 'Task was successfully updated.' }
+        format.html { redirect_to @task, notice: 'Task was successfully updated.' }
         format.json { render :show, status: :ok, location: @task }
       else
         format.js
-        # format.html { render :edit }
+        format.html { render :edit }
         format.json { render json: @task.errors, status: :unprocessable_entity }
       end
     end
@@ -59,7 +60,7 @@ class TasksController < ApplicationController
     @task.destroy
     respond_to do |format|
       format.js
-      # format.html { redirect_to tasks_url, notice: 'Task was successfully destroyed.' }
+      format.html { redirect_to tasks_url, notice: 'Task was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
